0f5e1ebd761e84dea1edf90efb389d6989618f1f
HariniG23 <harinignanam2305@gmail.com>
2018-01-06 10:31:48 +0000

    Initial commit

---
+++ src/main/java/HarryG-Amazon/App.java
@@ -0,0 +1,13 @@
+package HarryG-Amazon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
